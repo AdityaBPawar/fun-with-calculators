6726d4f0ed3b579c9c190a97ae739ce8c670b807
Aditya Pawar <apawar6@rockets.utoledo.edu>
2025-10-07 19:40:08 +0530

    header

--- Java/basic_calculator/most_basic_calculator.java
+++ Java/basic_calculator/most_basic_calculator.java
@@ -1,4 +1,18 @@
 package basic_calculator;
 
 public class most_basic_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the most basic calculator!");
+        System.out.println("What operation woould you like to do?");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int num = sc.nextInt();
+
+        sc.close();
+    }
 }
