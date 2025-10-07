a1529e8ce28959794b9270793547707933e3a095
Aditya Pawar <apawar6@rockets.utoledo.edu>
2025-10-07 19:42:02 +0530

    engine of the code/logic

--- Java/basic_calculator/most_basic_calculator.java
+++ Java/basic_calculator/most_basic_calculator.java
@@ -15,4 +15,17 @@ public class most_basic_calculator {
 
         sc.close();
     }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static int division(int a, int b) {
+        return a / b;
+    }
 }
