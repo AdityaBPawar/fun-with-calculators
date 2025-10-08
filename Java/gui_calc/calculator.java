ebe9465392ccc1db9f7ca32914f346e8aaacd84f
Aditya Pawar <apawar6@rockets.utoledo.edu>
2025-10-08 20:37:24 +0530

    basic window created

--- Java/gui_calc/calculator.java
+++ Java/gui_calc/calculator.java
@@ -7,4 +7,17 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class calculator {
+    int windowwidth = 360;
+    int windowheight = 540;
+
+    JFrame frame =  new JFrame("Calculator");
+
+    calculator(){
+      frame.setVisible(true);
+      frame.setSize(windowwidth, windowheight);
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(false);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setLayout(new BorderLayout());
+    }
 }
