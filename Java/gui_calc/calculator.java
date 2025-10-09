38a15d6d2a0231dcb7539dbf7e08e9316d3786c7
Aditya Pawar <apawar6@rockets.utoledo.edu>
2025-10-09 20:27:48 +0530

    color pallette added

--- Java/gui_calc/calculator.java
+++ Java/gui_calc/calculator.java
@@ -10,6 +10,11 @@ public class calculator {
     int windowwidth = 360;
     int windowheight = 540;
 
+    Color lilac = new Color(153, 85, 187);
+    Color pink = new Color(255, 105, 179);
+    Color yellow = new Color(255, 255, 153);
+    Color turquoise = new Color(43, 217, 198);
+
     JFrame frame =  new JFrame("Calculator");
 
     calculator(){
